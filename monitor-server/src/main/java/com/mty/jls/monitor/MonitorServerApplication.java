8b270bd97db86f6ef13fc14de2f475d8dfc697cf
蒋鹏 <jiangpeng@ppmoney.com>
2020-11-05 12:32:25 +0800

    refactor: 实现的功能是nacos服务上下线的钉钉消息推送

---
+++ monitor-server/src/main/java/com/mty/jls/monitor/MonitorServerApplication.java
@@ -0,0 +1,13 @@
+package com.mty.jls.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorServerApplication.class, args);
+    }
+
+}
---
+++ monitor-server/src/test/java/com/mty/jls/monitor/MonitorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mty.jls.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
